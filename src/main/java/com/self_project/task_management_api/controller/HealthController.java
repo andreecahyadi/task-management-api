05eae8d094a9eca6a043d0a33b887c0713485723
Andree Tanuwijaya <andreecahyadit@gmail.com>
2025-10-24 10:30:52 +0700

    update health controller

--- src/main/java/com/self_project/task_management_api/controller/HealthController.java
+++ src/main/java/com/self_project/task_management_api/controller/HealthController.java
@@ -5,11 +5,13 @@ import java.util.Map;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.Collections;
+
 @RestController
 public class HealthController {
 
     @GetMapping({"/", "/api/ping"})
     public Map<String, String> ping() {
-        return Map.of("status", "check hit api success.");
+        return Collections.singletonMap("status", "check hit api success.");
     }
 }
